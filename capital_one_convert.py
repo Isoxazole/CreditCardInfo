561132b951c714201f1df488e23c1ddceab33557
Will <wmorris1367@gmail.com>
2019-11-24 21:02:08 -0700

    Added sort by month function to class

--- capital_one_convert.py
+++ capital_one_convert.py
@@ -1,11 +1,12 @@
 import csv
 import datetime
+import calendar
 
 
 class CapitalOneConvert:
 
     # Initialize all data into a list and set the indexes for the different categories
-    def __init__(self, filename, date_index=(0, 1), desc_index=3, cat_index=4, debit_index=5, credit_index=6):
+    def __init__(self, filename, date_index=0, desc_index=3, cat_index=4, debit_index=5, credit_index=6):
         self.filename = filename
         self.data = []
         self.date_index = date_index
@@ -34,11 +35,11 @@ class CapitalOneConvert:
     # takes argument of data and indexes which need to have date converted
     def clean_date_format(self):
         for row in self.data:
-            for index in self.date_index:
-                try:
-                    row[index] = datetime.datetime.strptime(row[index], '%Y-%m-%d').strftime('%m/%d/%y')
-                except ValueError:
-                    raise ValueError("Incorrect format, should be YYYY-MM-DD")
+            date = row[self.date_index]
+            try:
+                row[self.date_index] = datetime.datetime.strptime(date, '%Y-%m-%d').strftime('%m/%d/%y')
+            except ValueError:
+                raise ValueError("Incorrect format, should be YYYY-MM-DD")
 
     def sort_trans_category(self):
         sorted_categories = {}
@@ -47,13 +48,26 @@ class CapitalOneConvert:
             sorted_categories.setdefault(category, []).append(row)
         return sorted_categories
 
-
+    def sort_by_month(self):
+        sorted_months = {}
+        for row in self.data:
+            date = row[self.date_index]
+            month_num = datetime.datetime.strptime(date, '%m/%d/%y').month
+            month = calendar.month_name[month_num]
+            sorted_months.setdefault(month, []).append(row)
+        return sorted_months
 shit = CapitalOneConvert("2019-01-20_transaction_download.csv")
 print(shit.get_count_categories())
 shit.clean_date_format()
 print(shit.data)
 data_sort_category = shit.sort_trans_category()
+data_sort_month = shit.sort_by_month()
 
-for key,value in data_sort_category.items():
+for key, value in data_sort_category.items():
     print(key)
     print(value, "\n")
+
+for key, value in data_sort_month.items():
+    print(key)
+    print(value, "\n")
+
